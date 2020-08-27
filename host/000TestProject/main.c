6aec415a3d0b1aa54d18a1044c28238ba2f58a9e
Xcode23 <joaofarinha10@hotmail.com>
2020-08-27 16:44:18 +0200

    host and target test project

---
+++ host/000TestProject/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 27, 2020
+ *      Author: jsf
+ */
+
+#include<stdio.h>
+
+int main(void){
+	printf("Hello world\n");
+
+	printf("Press 'Enter' to exit this application\n");
+
+	getchar();
+
+	return 0;
+}
